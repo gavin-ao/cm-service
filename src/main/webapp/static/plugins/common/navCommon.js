8c24179728d79f14588fb267a63ce9e79a5be7e5
sjh0824 <1204563557@qq.com>
2018-11-10 13:21:51 +0800

    根据账号权限 左侧导航栏展示与隐藏

--- src/main/webapp/static/plugins/common/navCommon.js
+++ src/main/webapp/static/plugins/common/navCommon.js
@@ -299,3 +299,12 @@ function returnIsNotInArray(arr, obj) {
     }
     return obj;
 }
+
+function isInArray(arr, value) {
+    for (var i = 0; i < arr.length; i++) {
+        if (value === arr[i]) {
+            return true;
+        }
+    }
+    return false;
+}
